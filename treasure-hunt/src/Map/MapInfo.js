6231bcc8a63d4b50d8cf17964015ac00171a64f5
Jacob McFaul <mcjakedude@gmail.com>
2019-07-28 12:32:35 -0700

    added a side box for mapInfo

---
+++ treasure-hunt/src/Map/MapInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class MapInfo extends Component {
+    render() {
+        return (
+            <div className="mapInfoWrapper">
+                
+            </div>
+        )
+    }
+}
+
+export default MapInfo
