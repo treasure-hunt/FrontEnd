ed83b299da9980b0a409cb1d5d2f6f3f74c8fa5a
t-tullis <t.tullis34@gmail.com>
2019-08-02 10:36:45 -0700

    added styling to popup and added 'treasure hunt' title

---
+++ treasure-hunt/src/Map/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Title(){
+    return(
+        <div className='game-title'>
+            <h1>Treasure Hunt</h1>
+        </div>
+    )
+}
+
+export default Title
